917cf153091ac8f82cac4596b627e821db5ff712
rsawejka <rsawejka@my.wctc.com>
2021-03-04 10:03:46 -0600

    finished step 3

--- hello-frontity/packages/my-first-theme/src/components/index.js
+++ hello-frontity/packages/my-first-theme/src/components/index.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { connect } from "frontity"
 import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
+import List from "./list"
+import Post from "./post"
+import Page from "./page"
 
 const Root = ({state}) => {
+    const data = state.source.get(state.router.link)
     return (
         <>
             <h1>Frontity Workshop</h1>
@@ -14,6 +19,14 @@ const Root = ({state}) => {
                 <br />
                 <Link link="/about-us">About Us</Link>
             </nav>
+            <hr />
+            <main>
+                <Switch>
+                    <List when={data.isArchive} />
+                    <Post when={data.isPost} />
+                    <Page when={data.isPage} />
+                </Switch>
+            </main>
         </>
     )
 }
