571dae1376fa0f85203c8d9c7ca7a983fd3d94b1
rsawejka <rsawejka@my.wctc.com>
2021-03-04 10:24:32 -0600

    finished 4th step

--- hello-frontity/packages/my-first-theme/src/components/list.js
+++ hello-frontity/packages/my-first-theme/src/components/list.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, styled } from "frontity"
 import Link from "@frontity/components/link"
 
 const List = ({ state }) => {
@@ -7,7 +7,8 @@ const List = ({ state }) => {
 
 
     return (
-        <div>
+        <Items>
+
             {data.items.map((item) => {
                 const post = state.source[item.type][item.id]
                 return (
@@ -17,8 +18,18 @@ const List = ({ state }) => {
                     </Link>
                 )
             })}
-        </div>
+        </Items>
     )
 }
 
 export default connect(List)
+
+const Items = styled.div`
+  & > a {
+    display: block;
+    margin: 6px 0;
+    font-size: 1.2em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`
